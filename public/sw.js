ecd8e9dd29318ee6d6dfd30c269dd47343e2985c
superawesomeman824-2032 <superawesomeman824@gmail.com>
2026-08-30 21:24:18 -0500

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+importScripts('/uv/uv.bundle.js');
+importScripts('/uv.config.js');
+importScripts('/uv/uv.sw.js');
+
+const sw = new UVServiceWorker();
+self.addEventListener('fetch', event => {
+    event.respondWith(sw.route(event));
+});
